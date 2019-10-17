ca2a9d09e1954286d182e516d9efa952efca4f4c
Robert Thompson <rthom2171@icloud.com>
2019-10-16 20:45:12 -0500

    Added Student and Helper landing page components

---
+++ src/components/HelperLandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelperLandingPage = () => {
+    return(
+        <div>
+            <h1>Helper Landing Page...</h1>
+        </div>
+    )
+}
+
+export default HelperLandingPage;
